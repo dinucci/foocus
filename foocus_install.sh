22f81c3c4650a083581ecbc7435954cc5862b3e6
Ale DN <dinucci@protonmail.ch>
2023-12-21 14:27:09 +0100

    Create foocus_install.sh

---
+++ foocus_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#https://github.com/lllyasviel/Fooocus
+
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+pip3 install -r requirements_versions.txt
+
+python3 entry_with_update.py --preset realistic
