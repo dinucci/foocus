0684164775eb332b98c2be97b0e8389b9284d216
Ale DN <dinucci@protonmail.ch>
2024-04-29 00:43:14 +0200

    Update foocus_install.sh

--- foocus_install.sh
+++ foocus_install.sh
@@ -4,6 +4,7 @@
 
 git clone https://github.com/lllyasviel/Fooocus.git
 cd Fooocus
+pip install opencv-python
 pip3 install -r requirements_versions.txt
 
 python3 entry_with_update.py --preset realistic
